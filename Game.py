be104634b180a4f50b59c7c5525076c67fd6c33d
chikyuugaugokidasita <ansvia@naver.com>
2025-10-01 18:51:19 +0900

    Game을 전체적으로 관리할 클래스 Game 생성

---
+++ Game.py
@@ -0,0 +1,23 @@
+import pico2d
+
+import pch
+
+class Game:
+    def __init__(self):
+        pass
+
+    def initialize(self):
+        #TODO / Initialize Managers
+        pass
+
+    def update(self, deltaTime):
+        #TODO / Update Managers
+        pass
+
+    def render(self):
+        #TODO / Render Managers
+        pass
+
+    def run(self):
+        #TODO / Game Loop
+        pass
