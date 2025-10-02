cfb4fa3f0dddeef5b0b7c805b2fe26d8f157d79d
chikyuugaugokidasita <ansvia@naver.com>
2025-10-02 14:41:21 +0900

    4개의 캐릭터의 부모클래스(임시)

---
+++ Character/Character.py
@@ -0,0 +1,16 @@
+import pico2d
+
+class Character:
+    def __init__ (self):
+        self.image = None
+        self.frame = 0
+
+
+    def initialize(self):
+        pass
+
+    def update(self, deltaTime):
+        pass
+
+    def render(self):
+        pass
