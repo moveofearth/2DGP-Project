3f60a6c00a7d3421bece2fc1f3870d7efb6ec06b
地球が動き出した <79787986+moveofearth@users.noreply.github.com>
2025-11-10 18:19:23 +0900

    fighter의 중단 강공격 완성

--- spriteManager.py
+++ spriteManager.py
@@ -63,6 +63,7 @@ class SpriteManager:
             'fastMiddleATK': [pico2d.load_image(str(base_path / 'fighter' / 'fastMiddleATK' / f'{i}.png')) for i in range(3)],
             'fastMiddleATK2': [pico2d.load_image(str(base_path / 'fighter' / 'fastMiddleATK' / f'{i}.png')) for i in range(3, 6)],
             'fastMiddleATK3': [pico2d.load_image(str(base_path / 'fighter' / 'fastMiddleATK' / f'{i}.png')) for i in range(6, 8)],
+            'strongMiddleATK': [pico2d.load_image(str(base_path / 'fighter' / 'strongMiddleATK' / f'{i}.png')) for i in range(3)],
             'fastLowerATK': [pico2d.load_image(str(base_path / 'fighter' / 'fastLowerATK' / f'{i}.png')) for i in range(4)],  # 0~3
             'fastUpperATK': [pico2d.load_image(str(base_path / 'fighter' / 'fastUpperATK' / f'{i}.png')) for i in range(6)]  # 0~5
         }
@@ -89,7 +90,8 @@ class SpriteManager:
             },
             'fighter': {
                 'fastMiddleATK': 'fastMiddleATK2',
-                'fastMiddleATK2': 'fastMiddleATK3'
+                'fastMiddleATK2': 'fastMiddleATK3',
+                'strongMiddleATK': 'fastMiddleATK'
             }
         }
 
@@ -111,11 +113,11 @@ class SpriteManager:
 
         # 연계가 없는 공격이나 마지막 연계 완료
         elif state in ['fastMiddleATK3', 'strongMiddleATK2', 'strongUpperATK2', 'strongLowerATK',
-                       'strongUpperATK', 'strongLowerATK', 'fastMiddleATK', 'fastLowerATK', 'fastUpperATK']:
+                       'strongUpperATK', 'strongLowerATK', 'fastMiddleATK', 'fastLowerATK', 'fastUpperATK', 'strongMiddleATK']:
             # 마지막 연계이거나 단일 공격 완료
             if (state == 'fastMiddleATK' and character_type == 'priest') or \
                (state in ['strongUpperATK', 'strongLowerATK'] and character_type == 'priest') or \
-               (state in ['fastLowerATK', 'fastUpperATK'] and character_type == 'fighter'):
+               (state in ['fastLowerATK', 'fastUpperATK', 'strongMiddleATK'] and character_type == 'fighter'):
                 # 단일 공격
                 self._end_attack(player_ref, is_player1)
                 return True
